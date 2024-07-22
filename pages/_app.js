ca22bb42a82ec05e660e7b05ebda46f2478cd0ec
Alex <alex@jetweels.com>
2024-07-22 14:02:46 +0700

    update vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import { AnimatePresence } from 'framer-motion';
 import Chakra from '../components/Chakra/chakra';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import '../styles/global.css';
 
@@ -14,7 +13,6 @@ function MyApp({ Component, pageProps, router }) {
     <Chakra cookies={pageProps.cookies}>
       <Component {...pageProps} key={router.route} />
       <Analytics />
-      <SpeedInsights />
     </Chakra>
   );
 }
